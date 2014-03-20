fe12181250f1b842000bd37ec3c942babd4b70ce
Timothée Peignier <timothee.peignier@tryphon.org>
2014-03-20 12:38:15 -0700

    allow to modify options

--- lib/platform-api/client.rb
+++ lib/platform-api/client.rb
@@ -1,10 +1,10 @@
 module PlatformAPI
   # Get a client configured with the specified OAuth token.
-  def self.connect_oauth(oauth_token)
+  def self.connect_oauth(oauth_token, options={})
     url = "https://api.heroku.com"
     default_headers = {'Accept' => 'application/vnd.heroku+json; version=3'}
     cache = Moneta.new(:File, dir: "#{Dir.home}/.heroku/platform-api")
-    options = {default_headers: default_headers, cache: cache}
+    options = {default_headers: default_headers, cache: cache}.merge(options)
     schema_json = File.read("#{File.dirname(__FILE__)}/schema.json")
     schema = Heroics::Schema.new(MultiJson.decode(schema_json))
     Heroics.oauth_client_from_schema(oauth_token, schema, url, options)
@@ -14,11 +14,11 @@ module PlatformAPI
   #
   # Always prefer `connect_oauth` unless there's a very good reason you must
   # use a non-OAuth API token.
-  def self.connect(token)
+  def self.connect(token, options={})
     url = "https://:#{token}@api.heroku.com"
     default_headers = {'Accept' => 'application/vnd.heroku+json; version=3'}
     cache = Moneta.new(:File, dir: "#{Dir.home}/.heroku/platform-api")
-    options = {default_headers: default_headers, cache: cache}
+    options = {default_headers: default_headers, cache: cache}.merge(options)
     schema_json = File.read("#{File.dirname(__FILE__)}/schema.json")
     schema = Heroics::Schema.new(MultiJson.decode(schema_json))
     Heroics.client_from_schema(schema, url, options)
