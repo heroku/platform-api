3fd10c078c177df5aa448ad4ebc908f25508065f
Jonathan Clem <jonathan@jclem.net>
2014-04-17 15:43:01 -0700

    allow specifying host in options

--- lib/platform-api/client.rb
+++ lib/platform-api/client.rb
@@ -15,12 +15,21 @@ module PlatformAPI
   # Always prefer `connect_oauth` unless there's a very good reason you must
   # use a non-OAuth API token.
   def self.connect(token, options={})
-    url = "https://:#{token}@api.heroku.com"
+    options = default_options.merge(options)
+    url = "https://:#{token}@#{options[:host]}"
     default_headers = {'Accept' => 'application/vnd.heroku+json; version=3'}
     cache = Moneta.new(:File, dir: "#{Dir.home}/.heroku/platform-api")
-    options = {default_headers: default_headers, cache: cache}.merge(options)
     schema_json = File.read("#{File.dirname(__FILE__)}/schema.json")
     schema = Heroics::Schema.new(MultiJson.decode(schema_json))
     Heroics.client_from_schema(schema, url, options)
   end
+
+  def self.default_options
+    {
+      default_headers: default_headers,
+      cache: cache,
+      host: "api.heroku.com"
+    }
+  end
+  private_class_method :default_options
 end
