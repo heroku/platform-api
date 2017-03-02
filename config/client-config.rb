ce85ee9d3e13f0ea69f18ee545b81bf9d6b55a1c
Dan Collis-Puro <dan@collispuro.net>
2017-03-02 16:50:24 -0500

    Revert "Revert "Use heroics 0.0.21, load the config and the schema file at runtime to correctly map schema attributes used in metaprogramming""
    This reverts commit e3efbe70cc78be98d2d93b73767a507787f8e032.

--- config/client-config.rb
+++ config/client-config.rb
@@ -4,12 +4,14 @@ require 'heroics'
 Heroics.default_configuration do |config|
   config.base_url = 'https://api.heroku.com'
   config.module_name = 'PlatformAPI'
-  config.schema_filepath = 'schema.json'
+  config.schema_filepath = File.join(File.expand_path('../..', __FILE__), 'schema.json')
 
   config.headers = { 'Accept' => 'application/vnd.heroku+json; version=3' }
   config.ruby_name_replacement_patterns = {
     /add[^a-z]+on/i => 'addon',
     /[\s-]+/ => '_',
   }
+  # This needs to be in single quotes to avoid interpolation during the client
+  # build
   config.cache_path = '#{Dir.home}/.heroics/platform-api'
 end
