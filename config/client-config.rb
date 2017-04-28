e8c6bf8aae151f71389134f3179861969dcfb1b5
geemus <geemus@gmail.com>
2017-04-28 15:38:26 -0500

    add user-agent

--- config/client-config.rb
+++ config/client-config.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 require 'heroics'
+require File.join(File.expand_path('../..', __FILE__), 'lib', 'platform-api', 'version.rb')
 
 Heroics.default_configuration do |config|
   config.base_url = 'https://api.heroku.com'
   config.module_name = 'PlatformAPI'
   config.schema_filepath = File.join(File.expand_path('../..', __FILE__), 'schema.json')
 
-  config.headers = { 'Accept' => 'application/vnd.heroku+json; version=3' }
+  config.headers = {
+    'Accept'      => 'application/vnd.heroku+json; version=3',
+    'User-Agent'  => "platform-api/#{PlatformAPI::VERSION}"
+  }
   config.ruby_name_replacement_patterns = {
     /add[^a-z]+on/i => 'addon',
     /[\s-]+/ => '_',
