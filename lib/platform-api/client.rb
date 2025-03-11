57a148be361a640d44cf28e9c3a7a04ff786fcbf
Dmytro (Dima) Kabanov <dkabanov@salesforce.com>
2025-03-11 15:09:48 -0700

    3.8.0

--- lib/platform-api/client.rb
+++ lib/platform-api/client.rb
@@ -83,7 +83,7 @@ module PlatformAPI
 
   # Get the default options.
   def self.default_options
-    default_headers = {"Accept"=>"application/vnd.heroku+json; version=3", "User-Agent"=>"platform-api/3.7.1"}
+    default_headers = {"Accept"=>"application/vnd.heroku+json; version=3", "User-Agent"=>"platform-api/3.8.0"}
     {
       default_headers: default_headers,
       url:             "https://api.heroku.com"
